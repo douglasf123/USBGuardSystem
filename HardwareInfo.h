6fe046efe1edfd7a7d4f7cb2ce76e7c3f7f66258
Douglas Figueiredo <37126170+douglasf123@users.noreply.github.com>
2026-07-21 17:00:48 -0300

    Delete HardwareInfo.h

--- HardwareInfo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-// Retorna o Serial Number físico do dispositivo associado à letra da unidade (ex: 'E')
-// Retorna o Serial Number fisico do dispositivo associado a letra da unidade (ex: 'E')
-std::string GetUsbSerialNumber(char driveLetter);
-
-// Retorna true se o dispositivo por tras da letra for conectado via barramento USB
-bool IsUsbBus(char driveLetter);
